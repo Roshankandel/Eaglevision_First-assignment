4895d924a86f0a86d86149e84c26ad3c2d649311
Roshan Kandel <roshankdl207@gmail.com>
2022-12-23 16:39:25 +0545

    fixed responsiveness issues

--- assets/script/script.js
+++ assets/script/script.js
@@ -14,9 +14,29 @@ function navToggle() {
 }
 
 $(window).scroll(function () {
-    if ($(this).scrollTop() > 0) {
+    if ($(this).scrollTop() > 30) {
         $("#mynav").addClass("scrolled");
     } else {
         $("#mynav").removeClass("scrolled");
     }
+});
+
+
+
+// for scroll to top button
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 600) {
+        $(".scrollup").fadeIn();
+    } else {
+        $(".scrollup").fadeOut();
+    }
+});
+$(".scrollup").click(function () {
+    $("html,body").animate({
+        scrollTop: 0
+    }, 600
+    );
+    return false;
+
+
 });
