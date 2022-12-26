a092fdb303d49555dc1ae7317e09970e36a72387
Roshan Kandel <roshankdl207@gmail.com>
2022-12-26 14:51:27 +0545

    responsive navigation

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,42 +1,50 @@
-const navToggler = document.querySelector(".nav-toggler");
-navToggler.addEventListener("click", navToggle);
+$(document).ready(function () {
 
-function navToggle() {
-    navToggler.classList.toggle("active");
-    const nav = document.querySelector(".nav");
-    nav.classList.toggle("open");
-    if (nav.classList.contains("open")) {
-        nav.style.maxHeight = nav.scrollHeight + "px";
-    }
-    else {
-        nav.removeAttribute("style");
-    }
-}
 
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 30) {
-        $("#mynav").addClass("scrolled");
-    } else {
-        $("#mynav").removeClass("scrolled");
-    }
-});
+    $(".nav-toggler").click(function () {
+        $(".nav-toggler").toggleClass("active");
+    });
+
+    $(".nav-toggler").click(function () {
+        $(".nav_items").toggleClass("nav_open");
+        $("ul li").toggleClass("nav_items_open");
+        $("#mynav").toggleClass("nav_open_active");
+    });
 
 
 
-// for scroll to top button
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 600) {
-        $(".scrollup").fadeIn();
-    } else {
-        $(".scrollup").fadeOut();
-    }
-});
-$(".scrollup").click(function () {
-    $("html,body").animate({
-        scrollTop: 0
-    }, 600
-    );
-    return false;
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 30) {
+            $("#mynav").addClass("scrolled");
+        } else {
+            $("#mynav").removeClass("scrolled");
+        }
+    });
 
 
+    //scroll down btn in hero section
+
+    $("#downClick").on("click", function () {
+        $("html,body").animate({
+            scrollTop: 550
+        });
+    });
+
+    // for scroll to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 600) {
+            $(".scrollup").fadeIn();
+        } else {
+            $(".scrollup").fadeOut();
+        }
+    });
+    $(".scrollup").click(function () {
+        $("html,body").animate({
+            scrollTop: 0
+        }, 600
+        );
+        return false;
+
+
+    });
 });
