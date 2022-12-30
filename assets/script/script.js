ccb755fe332b42070dea68265fc20ffa9469a787
Roshan Kandel <roshankdl207@gmail.com>
2022-12-30 17:07:46 +0545

    some changes

--- assets/script/script.js
+++ assets/script/script.js
@@ -62,4 +62,32 @@ $(document).ready(function () {
 
     });
 
+    // slick slider
+
+    $('.slider_container').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+
+        ]
+    });
+
 });
